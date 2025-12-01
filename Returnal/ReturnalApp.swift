cf4c6066ca2842807c62131215e0f3126993b3ff
swiftAndHandy <158059006+swiftAndHandy@users.noreply.github.com>
2025-12-01 09:32:08 +0100

    Initial Commit

---
+++ Returnal/ReturnalApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReturnalApp.swift
+//  Returnal
+//
+//  Created by Andre Veltens on 01.12.25.
+//
+
+import SwiftUI
+
+@main
+struct ReturnalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
