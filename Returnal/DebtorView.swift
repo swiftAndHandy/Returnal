1384e6d571e9f6ef9b0474babfdb8ebf18d891f3
swiftAndHandy <158059006+swiftAndHandy@users.noreply.github.com>
2025-12-02 20:53:39 +0100

    refactor: seperated Debtor-Details to an additional view

--- Returnal/DebtorView.swift
+++ Returnal/DebtorView.swift
@@ -8,11 +8,67 @@
 import SwiftUI
 
 struct DebtorView: View {
+    
+    private var debtor: Borrower
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading) {
+            HStack(alignment: .top , spacing: 16) {
+                VStack(alignment: .leading) {
+                    Text("Verliehen an:")
+                        .font(.caption)
+                    Text("\(debtor.firstName) \(debtor.lastName)")
+                        .font(.default.bold())
+                    if let address = debtor.address {
+                        Text("\(address.street ?? "")")
+                            .font(.subheadline)
+                        HStack {
+                            Text("\(address.zipCode ?? "")")
+                                .font(.subheadline)
+                            Text("\(address.city ?? "")")
+                                .font(.subheadline)
+                        }
+                        Text("\(address.country ?? "")")
+                            .font(.subheadline)
+                    }
+                }
+                Spacer()
+                VStack(alignment: .leading) {
+                    Text("Verliehen am:")
+                        .font(.caption)
+                    Text("\(debtor.dateOfBorrowing.formatted(date: .long, time: .omitted))")
+                }
+            }
+            if debtor.email != nil || debtor.phoneNumber != nil {
+                Divider()
+                    .padding(.bottom)
+                if let email = debtor.email {
+                    HStack {
+                        Text("E-Mail:")
+                        Text("\(email)")
+                    }
+                    .font(.subheadline)
+                }
+                if let phoneNumber = debtor.phoneNumber {
+                    HStack {
+                        Text("Telefon:")
+                        Text("\(phoneNumber)")
+                    }
+                    .font(.subheadline)
+                }
+            }
+        }
+        Divider()
     }
+    
+    init(debtor: Borrower) {
+        self.debtor = debtor
+    }
+    
 }
 
 #Preview {
-    DebtorView()
+    DebtorView(
+        debtor: Borrower(firstName: "Klaus", lastName: "Kleber")
+    )
 }
--- Returnal/ItemDetailsView.swift
+++ Returnal/ItemDetailsView.swift
@@ -76,53 +76,7 @@ struct ItemDetailsView: View {
                 Divider()
                 
                 if let debtor = item.debtor {
-                    VStack(alignment: .leading) {
-                        HStack(alignment: .top , spacing: 16) {
-                            VStack(alignment: .leading) {
-                                Text("Verliehen an:")
-                                    .font(.caption)
-                                Text("\(debtor.firstName) \(debtor.lastName)")
-                                    .font(.default.bold())
-                                if let address = debtor.address {
-                                    Text("\(address.street ?? "")")
-                                        .font(.subheadline)
-                                    HStack {
-                                        Text("\(address.zipCode ?? "")")
-                                            .font(.subheadline)
-                                        Text("\(address.city ?? "")")
-                                            .font(.subheadline)
-                                    }
-                                    Text("\(address.country ?? "")")
-                                        .font(.subheadline)
-                                }
-                            }
-                            Spacer()
-                            VStack(alignment: .leading) {
-                                Text("Verliehen am:")
-                                    .font(.caption)
-                                Text("\(debtor.dateOfBorrowing.formatted(date: .long, time: .omitted))")
-                            }
-                        }
-                        if debtor.email != nil || debtor.phoneNumber != nil {
-                            Divider()
-                                .padding(.bottom)
-                            if let email = debtor.email {
-                                HStack {
-                                    Text("E-Mail:")
-                                    Text("\(email)")
-                                }
-                                .font(.subheadline)
-                            }
-                            if let phoneNumber = debtor.phoneNumber {
-                                HStack {
-                                    Text("Telefon:")
-                                    Text("\(phoneNumber)")
-                                }
-                                .font(.subheadline)
-                            }
-                        }
-                    }
-                    Divider()
+                    DebtorView(debtor: debtor)
                 }
             
                 
