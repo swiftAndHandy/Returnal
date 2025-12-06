d0441095de2f823231b819a58636219f2713c83a
swiftAndHandy <158059006+swiftAndHandy@users.noreply.github.com>
2025-12-06 13:59:27 +0100

    fix: removed  doubled item description in borrowerHistory

--- Returnal/BorrowerHistoryView.swift
+++ Returnal/BorrowerHistoryView.swift
@@ -44,16 +44,6 @@ struct BorrowerHistoryView: View {
 //                                        .foregroundStyle(checkDeadlineExpiration(borrower: borrower))
                                 }
                                 
-                                if let description = borrower.borrowedItemDetails {
-                                    if !description.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty {
-                                        Text("""
-                                        Beschreibung: 
-                                        \(description)
-                                        """)
-                                        .padding(.top)
-                                    }
-                                }
-                                
                                 if index < sortedDebtors.count - 1 {
                                     Divider()
                                 }
